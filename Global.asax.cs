4240b9fad17e1ea17ed504ad8557f1a0798d88a3
Yunus Demirpolat <yunusdemirpolatt@gmail.com>
2019-12-02 14:54:37 +0300

    add cronjob

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Web;
+using System.Web.Caching;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -10,12 +13,57 @@ namespace EnglishGram
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private const string DummyCacheItemKey = "GagaGuguGigi";
+        private const string DummyPageUrl = "https://localhost:44323/";
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            RegisterCacheEntry();
+        }
+        private bool RegisterCacheEntry()
+        {
+            if (null != HttpContext.Current.Cache[DummyCacheItemKey]) return false;
+
+            HttpContext.Current.Cache.Add(DummyCacheItemKey, "Test", null,
+                DateTime.MaxValue, TimeSpan.FromMinutes(1),
+                CacheItemPriority.Normal,
+                new CacheItemRemovedCallback(CacheItemRemovedCallback));
+
+            return true;
+        }
+
+        public void CacheItemRemovedCallback(string key,
+            object value, CacheItemRemovedReason reason)
+        {
+            Debug.WriteLine("Cache item callback: " + DateTime.Now.ToString());
+            HitPage();
+            // Do the service works
+
+            //DoWork();
+        }
+
+        private void HitPage()
+        {
+            WebClient client = new WebClient();
+            client.DownloadData(DummyPageUrl);
+        }
+
+        protected void Application_BeginRequest(Object sender, EventArgs e)
+        {
+            // If the dummy page is hit, then it means we want to add another item
+
+            // in cache
+
+            if (HttpContext.Current.Request.Url.ToString() == DummyPageUrl)
+            {
+                // Add the item in cache and when succesful, do the work.
+
+                RegisterCacheEntry();
+            }
         }
     }
 }
